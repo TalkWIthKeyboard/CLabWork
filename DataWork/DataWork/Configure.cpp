ea96e1f2ce29dc21cd3e0d6fc73dfeaea3aa1f29
wei song <wei song>
2016-06-01 18:12:06 +0800

    完善了Configure类的代码

---
+++ DataWork/DataWork/Configure.cpp
@@ -0,0 +1,42 @@
+#include "Configure.h"
+
+extern map<string, int> familyCodeNum;
+extern map<string, string> featureToFamily;
+
+Configure::Configure()
+{
+}
+
+Configure::~Configure()
+{
+}
+
+void Configure::featureCodeSort()
+{
+	string nowSort[1000];
+
+	for (int i = 0; i < featureArr.size(); i++)
+	{
+		nowSort[familyCodeNum[featureToFamily[featureArr[i]]]] = featureArr[i];
+	}
+
+	for (int i = 0; i < featureArr.size(); i++)
+	{
+		featureArr[i] = nowSort[i];
+	}
+}
+
+bool operator<(const Configure& con1, const Configure& con2)
+{
+	for (int i = 0; i < con1.featureArr.size(); i++)
+	{
+		if (con1.featureArr[i] < con2.featureArr[i])
+		{
+			return true;
+		}
+		if (con1.featureArr[i] > con2.featureArr[i])
+		{
+			return false;
+		}
+	}
+}
--- DataWork/DataWork/Configure.h
+++ DataWork/DataWork/Configure.h
@@ -2,21 +2,21 @@
 #include <cstring>
 #include <string>
 #include <vector>
+#include <map>
+
+using namespace std;
 
 class Configure
 {
+private:
+
 public:
 	Configure();
 	~Configure();
 	vector<string> featureArr;
-private:
-
+public:
+	//配置类中排序
+	void featureCodeSort();
+	//重载小于符号
+	friend bool operator<(const Configure& con1, const Configure& con2);
 };
-
-Configure::Configure()
-{
-}
-
-Configure::~Configure()
-{
-}
